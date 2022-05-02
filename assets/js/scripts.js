b32e5048ca4735ec6a1cff4275dd7a82aebae162
ViniciusNeves12 <viniciusneves1312@hotmail.com>
2022-05-02 00:30:44 -0300

    JS fundamentos web

---
+++ assets/js/scripts.js
@@ -0,0 +1,43 @@
+let nome = window.document.getElementById('nome');
+let email = window.document.querySelector('#email');
+let assunto = window.document.querySelector('#assunto');
+
+nome.style.width = '100%';
+email.style.width = '100%';
+assunto.style.width = '100%';
+
+function validaNome(){
+    let txtNome = document.querySelector("#txtNome")
+
+    if(nome.value.length < 3){
+        txtNome.innerHTML= "Nome inválido";
+        txtNome.style.color = 'red'
+    }else{
+        txtNome.innerHTML= "Nome válido";
+        txtNome.style.color = 'green'
+    }
+}
+
+function validaEmail(){
+    let txtEmail = document.querySelector("#txtEmail")
+
+    if(email.value.indexOf('@')== -1 || email.value.indexOf('.')== -1){
+        txtEmail.innerHTML= "Email inválido";
+        txtEmail.style.color = 'red'
+    }else{
+        txtEmail.innerHTML= "Email válido";
+        txtEmail.style.color = 'green'
+    }
+}
+
+function validaAssunto(){
+    let txtAssunto = document.querySelector("#txtAssunto")
+
+    if(txtAssunto.value.length >= 100){
+        txtAssunto.innerHTML= "Email inválido";
+        txtAssunto.style.color = 'red'
+    }else{
+        txtAssunto.innerHTML= "Email válido";
+        txtAssunto.style.color = 'green'
+    }
+}
